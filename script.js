483add1906e2804bbca3251b91a4e725798f34a5
Quim Aguirre <quim.aguirre@hotmail.com>
2025-03-20 17:48:03 +0100

    feat(script.js): included code to smooth the scrolling from navbar links

--- script.js
+++ script.js
@@ -19,3 +19,14 @@ function countdown() {
 // Update countdown every second
 setInterval(countdown, 1000);
 countdown();
+
+// Smooth scrolling for navbar links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
